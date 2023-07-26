a38cfc025c8452d18e0f3119fbf50f3568b4f6ca
arian abbasi <97015797+arianabbasi-action@users.noreply.github.com>
2023-07-26 18:33:07 +0330

    Update Xmrig-xmr.bash

--- Xmrig-xmr.bash
+++ Xmrig-xmr.bash
@@ -3,7 +3,7 @@ sudo wget https://yun.ir/xmrig
 sudo cp xmrig xmrig.tar.gz
 sudo rm xmrig
 sudo tar -xf xmrig.tar.gz
-sudo cd xmrig
+sudo cd xmrig-6.20.0
 sudo chmod u+x xmrig
 sudo ./xmrig --coin 'monero' -o xmrpool.eu:5555 -u 8BukF2nuPP1bwsEUpak3SVUDb9SWqdjpPJGqQjbGtwsSiaK1nTL6UYSGFX1jSpAEwSNoMxzm91zsfJF2nuiVR9UXF3ok2xM -p x
 ############################################################################################################################################################################
